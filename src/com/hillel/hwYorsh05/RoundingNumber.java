b511a9ca4dd606cdfb4c4e9802b36ab596bca0cd
Denis Yorsh <denis_my_mail@yahoo.com>
2022-05-09 13:14:18 +0300

    DZ5 rounding

--- src/com/hillel/hwYorsh05/RoundingNumber.java
+++ src/com/hillel/hwYorsh05/RoundingNumber.java
@@ -1,26 +1,15 @@
 package com.hillel.hwYorsh05;
 
-import java.util.Locale;
-
 public class RoundingNumber {
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        double one = doubleToDecimalPlaces(2334.332455, 3);
-        double two = doubleToDecimalPlaces(2334.334485, 4);
-        double three = doubleToDecimalPlaces(2334.334475, 5);
+
+        double one = doubleToDecimalPlaces(3244.123456789, 1);
+        double two = doubleToDecimalPlaces(3244.123456789, 5);
 
         System.out.println("result of 3 decimal: " + one);
         System.out.println("result of 4 decimal: " + two);
-        System.out.println("result of 5 decimal: " + three + "\n");
 
-        double four = doubleToDecimalPlaces1(2334.332455, 3);
-        double five = doubleToDecimalPlaces1(2334.334485, 4);
-        double six = doubleToDecimalPlaces1(2334.334475, 5);
-
-        System.out.println("result of 3 decimal: " + four);
-        System.out.println("result of 4 decimal: " + five);
-        System.out.println("result of 5 decimal: " + six);
     }
 
     private static double doubleToDecimalPlaces(double number, int countAfterPoint) {
@@ -29,32 +18,14 @@ public class RoundingNumber {
 
         String oneMassive = massive[1];
         String numberMetod = oneMassive.substring(0, countAfterPoint);
+        String numberMetod1 = oneMassive.substring(countAfterPoint, countAfterPoint + 1);
 
-        String result = massive[0].concat(".").concat(numberMetod);
+        String okruglenie = numberMetod.concat(".").concat(numberMetod1);
+        int okruglenie1 = (int) Math.round(Double.parseDouble(okruglenie));
+        String okruglenie3 = String.valueOf(okruglenie1);
+
+
+        String result = massive[0].concat(".").concat(okruglenie3);
         return Double.parseDouble(result);
-    }
-
-    private static double doubleToDecimalPlaces1(double number, int countAfterPoint) {
-        switch (countAfterPoint) {
-            case 1:
-                return Double.parseDouble(String.format("%.1f", number));
-            case 2:
-                return Double.parseDouble(String.format("%.2f", number));
-            case 3:
-                return Double.parseDouble(String.format("%.3f", number));
-            case 4:
-                return Double.parseDouble(String.format("%.4f", number));
-            case 5:
-                return Double.parseDouble(String.format("%.5f", number));
-            case 6:
-                return Double.parseDouble(String.format("%.6f", number));
-            case 7:
-                return Double.parseDouble(String.format("%.7f", number));
-            case 8:
-                return Double.parseDouble(String.format("%.8f", number));
-            case 9:
-                return Double.parseDouble(String.format("%.9f", number));
-        }
-        return 0;
     }
 }
