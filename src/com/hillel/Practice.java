c8cd9a585b0388910b87df4fd67f06b8841718d7
Denis Yorsh <denis_my_mail@yahoo.com>
2022-05-21 14:21:35 +0300

    Practice

--- src/com/hillel/Practice.java
+++ src/com/hillel/Practice.java
@@ -28,5 +28,47 @@ public class Practice {
         Arrays.sort(array, Collections.reverseOrder());
         System.out.println(Arrays.toString(array));
         System.out.println("-----------------------------------------------------------------");
+
+        int[][] dimArray = {{5, 8, 3, 2, 6, 7}, {3, 8, 0, 1, 7, 9, 2}, {2, 1, 4}, {3, 5, 8, 0}};
+        for (int i = 0; i < dimArray.length; i++) {
+            for (int j = 0; j < dimArray[i].length; j++) {
+                System.out.print(dimArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < dimArray.length; i++) {
+            System.out.println(Arrays.toString(dimArray[i]));
+        }
+        System.out.println();
+        int[][] arrayN = new int[3][3];
+        for (int i = 0; i < arrayN.length; i++) {
+            for (int j = 0; j < arrayN[i].length; j++) {
+                if (i == j) {
+                    arrayN[i][j] += 1;
+                }
+                System.out.print(arrayN[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int[][] dimArra = {{5, 8, 3, 2, 6}, {3, 8, 0, 1, 7}};
+        for (int i = 0; i < dimArra.length; i++) {
+            for (int j = 0; j < dimArra[i].length; j++) {
+                System.out.print(dimArra[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int[][] dimArrayNew = new int[dimArra.length][dimArra[0].length];
+        for (int i = 0; i < dimArra[0].length; i++) {
+            for (int j = 0; j < dimArra.length; j++) {
+                int value = dimArra[j][i];
+                dimArrayNew[j][i] = value;
+                System.out.print(dimArrayNew[j][i] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-----------------------------------------------------------------");
     }
 }
