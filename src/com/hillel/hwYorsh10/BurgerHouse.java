480db145e12d5e80af41b51beded742807114ff2
Denis Yorsh <denis_my_mail@yahoo.com>
2022-05-27 12:46:31 +0300

    hw10

--- src/com/hillel/hwYorsh10/BurgerHouse.java
+++ src/com/hillel/hwYorsh10/BurgerHouse.java
@@ -4,6 +4,10 @@ public class BurgerHouse {
 
     public static void main(String[] args) {
 
+        extracted();
+    }
+
+    private static void extracted() {
         Burger burgerRegular = new Burger(1, 75, 50, 25, 35);
         Burger burgerWithoutMayonnaise = new Burger(1, 75, 50, 25);
         Burger burgerDoubleMeat = new Burger(1, 75, 75, 50, 25, 35);
