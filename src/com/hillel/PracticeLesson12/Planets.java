53c1dd351caf22006bf2b406d7c88856f3f5809f
Denis Yorsh <denis_my_mail@yahoo.com>
2022-06-06 11:40:45 +0300

    Practice Lesson 12

---
+++ src/com/hillel/PracticeLesson12/Planets.java
@@ -0,0 +1,17 @@
+package com.hillel.PracticeLesson12;
+
+public enum Planets {
+    MERCURY(123),
+    VENUS(456),
+    EARTH(789),
+    MARS(1234),
+    JUPITER(5678),
+    SATURN(9012),
+    URANUS(3456),
+    NEPTUNE(7890),
+    PLUTO(12345);
+
+    Planets(int weight) {
+
+    }
+}
