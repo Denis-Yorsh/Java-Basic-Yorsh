cc056100a66d826767328cde06a2e789e4d32283
Denis Yorsh <denis_my_mail@yahoo.com>
2022-06-07 19:00:06 +0300

    DZ 13

--- src/com/hillel/hwYorsh13/StringBuilderMethod.java
+++ src/com/hillel/hwYorsh13/StringBuilderMethod.java
@@ -1,5 +1,7 @@
 package com.hillel.hwYorsh13;
 
+import java.util.Arrays;
+
 public class StringBuilderMethod {
 
     public static void main(String[] args) {
@@ -13,12 +15,31 @@ public class StringBuilderMethod {
         //StringBuilder translate String
         String toStrings = string.toString();
         //translate char[] method uniqueChars
-        char[] uniqueChars = uniqueChars(toStrings);
+        char[] uniqueChars = uniqueChars(toStrings, toStrings.length());
         //display on screen
-        System.out.println(uniqueChars);
+        System.out.println(Arrays.toString(uniqueChars));
     }
 
-    private static char[] uniqueChars(String s) {
-        return s.toCharArray();
+    private static char[] uniqueChars(String s, int length) {
+        char[] array = s.toCharArray();
+        Arrays.sort(array);
+        System.out.println(array);
+
+        if (length == 0 || length == 1) {
+            return s.toCharArray();
+        }
+
+        char[] tempA = new char[length];
+        char j = 0;
+        for (int i = 0; i < length - 1; i++) {
+            if (array[i] != array[i + 1]) {
+                tempA[j++] = array[i];
+            }
+        }
+        tempA[j++] = array[length-1];
+        for (int i=0; i<j; i++){
+            array[i] = tempA[i];
+        }
+        return tempA;
     }
 }
