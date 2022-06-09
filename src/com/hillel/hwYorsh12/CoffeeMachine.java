09784b1a049999e273f4c22f5407a3f5d18e6008
Denis Yorsh <denis_my_mail@yahoo.com>
2022-06-09 12:17:05 +0300

    DZ 12

--- src/com/hillel/hwYorsh12/CoffeeMachine.java
+++ src/com/hillel/hwYorsh12/CoffeeMachine.java
@@ -5,14 +5,50 @@ import java.util.Scanner;
 public class CoffeeMachine {
 
     public static void main(String[] args) {
-        Scanner enter = new Scanner(System.in);
-        // beverage selection
-        System.out.println("Choose a drink:\nCoffee, Tea, Mineral, Mahito, Lemonade or Coca_cola");
-        String stringEnter = enter.nextLine().toUpperCase();
-        // choice of drink amount
-        System.out.println("Enter the amount of this drink");
-        int intEnter = enter.nextInt();
-        //send user data to the method cooking
-        MethodsDrinksMachine.cooking(stringEnter, intEnter);
+        for (;;) {
+            // beverage selection
+            String stringEnter = getInputIntDigitsUserString
+                    ("Choose a drink:\nCoffee, Tea, Mineral, Mahito, Lemonade or Coca_cola");
+            // choice of drink amount
+            int intEnter = getInputIntDigitsUserInt("Enter the amount of this drink");
+            //send user data to the method cooking
+            MethodsDrinksMachine.cooking(stringEnter, intEnter);
+        }
+    }
+
+    private static String getInputIntDigitsUserString(String messageUser) {
+        boolean inputDigits = false;
+        String a = "";
+        while (!inputDigits) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(messageUser);
+
+            if (scanner.hasNextLine()) {
+                a = scanner.nextLine().toUpperCase();
+                inputDigits = true;
+
+            } else {
+                System.out.println("Try again");
+            }
+        }
+        return a;
+    }
+
+    private static int getInputIntDigitsUserInt(String messageUser) {
+        boolean inputDigits = false;
+        int a = 0;
+        while (!inputDigits) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(messageUser);
+
+            if (scanner.hasNextInt()) {
+                a = scanner.nextInt();
+                inputDigits = true;
+
+            } else {
+                System.out.println("Try again");
+            }
+        }
+        return a;
     }
 }
